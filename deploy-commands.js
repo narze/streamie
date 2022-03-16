0ef2d76288f838095d8bdf2a1f14dfe05a2bf579
Manassarn Manoonchai <manassarn@gmail.com>
2022-03-16 23:45:43 +0700

    refactor: separate commands into each file

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("@discordjs/builders")
+const fs = require("node:fs")
 const { REST } = require("@discordjs/rest")
 const { Routes } = require("discord-api-types/v9")
 require("dotenv").config()
@@ -9,17 +9,27 @@ const { clientId, guildId, token } = {
   token: process.env.DISCORD_TOKEN,
 }
 
-const commands = [
-  new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Replies with pong!"),
-  new SlashCommandBuilder()
-    .setName("server")
-    .setDescription("Replies with server info!"),
-  new SlashCommandBuilder()
-    .setName("user")
-    .setDescription("Replies with user info!"),
-].map((command) => command.toJSON())
+// const commands = [
+//   new SlashCommandBuilder()
+//     .setName("ping")
+//     .setDescription("Replies with pong!"),
+//   new SlashCommandBuilder()
+//     .setName("server")
+//     .setDescription("Replies with server info!"),
+//   new SlashCommandBuilder()
+//     .setName("user")
+//     .setDescription("Replies with user info!"),
+// ].map((command) => command.toJSON())
+
+const commands = []
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"))
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
+  commands.push(command.data.toJSON())
+}
 
 const rest = new REST({ version: "9" }).setToken(token)
 
--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const { Client, Intents } = require("discord.js")
+const fs = require("node:fs")
+const { Client, Collection, Intents } = require("discord.js")
 require("dotenv").config()
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+
+client.commands = new Collection()
+
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"))
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command)
+}
 
 // When the client is ready, run this code (only once)
 client.once("ready", () => {
@@ -12,18 +26,18 @@ client.once("ready", () => {
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return
 
-  const { commandName } = interaction
+  const command = client.commands.get(interaction.commandName)
 
-  if (commandName === "ping") {
-    await interaction.reply("Pong!")
-  } else if (commandName === "server") {
-    await interaction.reply(
-      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
-    )
-  } else if (commandName === "user") {
-    await interaction.reply(
-      `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
-    )
+  if (!command) return
+
+  try {
+    await command.execute(interaction)
+  } catch (error) {
+    console.error(error)
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    })
   }
 })
 
