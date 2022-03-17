bb11e08cdc9147ddd6b8b5aac9c405d636a62a8c
Manassarn Manoonchai <manassarn@gmail.com>
2022-03-18 00:24:38 +0700

    feat: add tmi.js & simple twitch bot

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require("node:fs")
 const { Client, Collection, Intents } = require("discord.js")
 require("dotenv").config()
 require("./ping-server")
+require("./twitch")
 
 // Create a new client instance
 const client = new Client({
---
+++ twitch.js
@@ -0,0 +1,20 @@
+const tmi = require("tmi.js")
+const client = new tmi.Client({
+  options: { debug: true, messagesLogLevel: "info" },
+  connection: {
+    reconnect: true,
+    secure: true,
+  },
+  identity: {
+    username: "narzeBOT",
+    password: process.env.TWITCH_OAUTH_TOKEN,
+  },
+  channels: ["narzeLIVE"],
+})
+client.connect().catch(console.error)
+client.on("message", (channel, tags, message, self) => {
+  if (self) return
+  if (message.toLowerCase() === "!hello") {
+    client.say(channel, `@${tags.username}, heya!`)
+  }
+})
