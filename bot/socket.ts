b49109ed877dc696f341c249e389ed35904d1ff1
Manassarn Manoonchai <manassarn@gmail.com>
2022-03-29 23:19:31 +0700

    Remove unused code

--- bot/socket.ts
+++ bot/socket.ts
@@ -8,16 +8,6 @@ export default function socket() {
     },
   })
 
-  io.on("connection", (socket) => {
-    // send a message to the client
-    socket.emit("hello from server", 1, "2", { 3: Buffer.from([4]) })
-
-    // receive a message from the client
-    socket.on("hello from client", (...args) => {
-      // ...
-      console.log({ args })
-    })
-  })
   console.log("started socket server at port 8000")
 
   return io
--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -11,15 +11,6 @@
   onMount(() => {
     const socket = io("ws://localhost:8080")
 
-    // send a message to the server
-    socket.emit("hello from client", 5, "6", { 7: Uint8Array.from([8]) })
-
-    // receive a message from the server
-    socket.on("hello from server", (...args) => {
-      // ...
-      console.log({ args })
-    })
-
     socket.on("message", ({ message, username }) => {
       messages = [...messages, `${username}: ${message}`]
 
