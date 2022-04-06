b7d825b3776fc170e4c69ff8aa1e77d4a71e209f
Manassarn Manoonchai <manassarn@gmail.com>
2022-04-06 13:35:17 +0700

    feat: ตกใจdiscord

---
+++ bot/twitch-commands/discord.ts
@@ -0,0 +1,10 @@
+import { ITwitchCommand } from "../types"
+
+const discord: ITwitchCommand = {
+  name: "!discord",
+  execute: async (client, channel, _tags, _message) => {
+    client.say(channel, `discord.narze.live`)
+  },
+}
+
+export default discord
