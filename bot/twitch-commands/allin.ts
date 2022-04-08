00438e3f33047c1072c9a5e1e78180a500b4ebfc
Manassarn Manoonchai <manassarn@gmail.com>
2022-04-09 00:47:11 +0700

    feat: !allin

---
+++ bot/twitch-commands/allin.ts
@@ -0,0 +1,47 @@
+import { ITwitchCommand } from "../types"
+import { isError, gacha as gachaFn } from "../invest"
+import prisma from "../prisma"
+
+const allin: ITwitchCommand = {
+  name: "!allin",
+  execute: async (client, channel, tags, _message, misc) => {
+    const name = tags.username!.toLowerCase()
+    const user = await prisma.user.findUnique({ where: { name } })
+
+    if (!user) {
+      return
+    }
+
+    let amount = user.coin
+
+    const gachaResult = await gachaFn(name, amount)
+
+    if (amount == 0 || isError(gachaResult)) {
+      await client.say(
+        channel,
+        `@${name} มี $OULONG ไม่พอ! (มีอยู่ ${user!.coin} $OULONG).`
+      )
+    } else {
+      if (gachaResult.data.state == "win") {
+        await client.say(
+          channel,
+          `@${name} ลงทุน ${gachaResult.data.bet} -> ได้ผลตอบแทน ${gachaResult.data.win} $OULONG (${gachaResult.data.balance}).`
+        )
+      } else if (gachaResult.data.state == "lose") {
+        // TODO: remove this
+
+        await client.say(
+          channel,
+          `@${name} ลงทุน ${gachaResult.data.bet} $OULONG -> แตก! (${gachaResult.data.balance}).`
+        )
+      }
+
+      misc?.io?.sockets.emit("gacha", {
+        data: gachaResult.data,
+        name: name,
+      })
+    }
+  },
+}
+
+export default allin
