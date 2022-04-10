2fd52c8bee11f90b2192e5ed359a74cb6eff64f1
Manassarn Manoonchai <manassarn@gmail.com>
2022-04-11 00:32:02 +0700

    feat: use redis instead of in-memory isPaused

--- bot/twitch.ts
+++ bot/twitch.ts
@@ -1,12 +1,17 @@
 import tmi from "tmi.js"
-import { ITwitchCommand } from "./types"
 import fs from "node:fs"
-import axios from "axios"
+// import axios from "axios"
+import { createClient } from "redis"
+
+import { ITwitchCommand } from "./types"
 import socket from "./socket-client"
 
 const io = socket()
+const redisClient = createClient({
+  url: process.env.REDIS_URL,
+})
 
-let isPaused = false
+redisClient.on("error", (err) => console.log("Redis Client Error", err))
 
 let interval
 
@@ -25,6 +30,7 @@ export default function twitch() {
   })
 
   client.connect().catch(console.error)
+  redisClient.connect().catch(console.error)
 
   const commands = new Map<string, ITwitchCommand>()
 
@@ -37,64 +43,60 @@ export default function twitch() {
     commands.set(command.name!, command)
   }
 
-  interval = setInterval(async () => {
-    const chattersResponse = await axios.get(
-      "https://tmi.twitch.tv/group/user/narzelive/chatters",
-      { responseType: "json" }
-    )
+  // interval = setInterval(async () => {
+  //   const chattersResponse = await axios.get(
+  //     "https://tmi.twitch.tv/group/user/narzelive/chatters",
+  //     { responseType: "json" }
+  //   )
 
-    const moderators = chattersResponse.data.chatters.moderators
+  //   const moderators = chattersResponse.data.chatters.moderators
 
-    // if "narzebotdev" is offline, unpause automatically
-    if (!moderators.includes("narzebotdev")) {
-      // TODO: Find better way to periodially inform prod bot to pause
-      if (process.env.NODE_ENV !== "production") {
-        await client.say("narzelive", `!pause`)
-        return
-      }
+  //   // if "narzebotdev" is offline, unpause automatically
+  //   if (!moderators.includes("narzebotdev")) {
+  //     // TODO: Find better way to periodially inform prod bot to pause
+  //     if (process.env.NODE_ENV !== "production") {
+  //       await client.say("narzelive", `!pause`)
+  //       return
+  //     }
 
-      if (isPaused) {
-        isPaused = false
-        client.say("narzelive", `isPaused: ${isPaused}`)
-      }
-    }
-  }, 60000)
+  //     if (isPaused) {
+  //       isPaused = false
+  //       client.say("narzelive", `isPaused: ${isPaused}`)
+  //     }
+  //   }
+  // }, 60000)
 
-  client.on("join", (channel, _name, self) => {
-    if (!self) {
-      return
-    }
+  // Dev mode - set streamie-dev-mode to true
+  if (process.env.NODE_ENV !== "production") {
+    redisClient.set("streamie-dev-mode", "true")
 
-    // Dev mode - send message to @narzebot to pause receiving commands
-    if (process.env.NODE_ENV !== "production") {
-      client.say(channel, "!pause")
-    }
-  })
+    // client.say(channel, "!pause")
+  }
 
   client.on("message", async (channel, tags, message, self) => {
     if (self) return
 
-    // Production bot pause
-    // TODO: refactor to twitch-commands/pause
-    if (
-      ["narzelive", "narzebotdev"].includes(tags.username!.toLowerCase()) &&
-      message.includes("!pause") &&
-      process.env.NODE_ENV === "production"
-    ) {
-      isPaused = true
-      client.say(channel, `isPaused: ${isPaused}`)
-      return
-    }
+    // // Production bot pause
+    // // TODO: refactor to twitch-commands/pause
+    // if (
+    //   ["narzelive", "narzebotdev"].includes(tags.username!.toLowerCase()) &&
+    //   message.includes("!pause") &&
+    //   process.env.NODE_ENV === "production"
+    // ) {
+    //   isPaused = true
+    //   client.say(channel, `isPaused: ${isPaused}`)
+    //   return
+    // }
 
-    if (
-      ["narzelive", "narzebotdev"].includes(tags.username!.toLowerCase()) &&
-      message.includes("!unpause") &&
-      process.env.NODE_ENV === "production"
-    ) {
-      isPaused = false
-      client.say(channel, `isPaused: ${isPaused}`)
-      return
-    }
+    // if (
+    //   ["narzelive", "narzebotdev"].includes(tags.username!.toLowerCase()) &&
+    //   message.includes("!unpause") &&
+    //   process.env.NODE_ENV === "production"
+    // ) {
+    //   isPaused = false
+    //   client.say(channel, `isPaused: ${isPaused}`)
+    //   return
+    // }
 
     const isCommaCommand = message.startsWith(",")
     const isBangCommand = message.startsWith("!")
@@ -145,27 +147,33 @@ export default function twitch() {
     }
 
     // Production bot skip !commands if paused
-    if (isPaused && process.env.NODE_ENV === "production") {
-      return
+    if (process.env.NODE_ENV === "production") {
+      const value = await redisClient.get("streamie-dev-mode")
+
+      if (value === "true") {
+        return
+      }
     }
 
     await command.execute(client, channel, tags, message, { io })
   })
 
-  async function disconnect() {
-    await client.say("narzeLIVE", "!unpause")
+  async function disconnect(exitCode: number = 0) {
+    await redisClient.set("streamie-dev-mode", "false")
+
+    // await client.say("narzeLIVE", "!unpause")
 
     console.log("disconnect", await client.disconnect())
 
     clearInterval(interval)
 
-    process.exit(0)
+    process.exit(exitCode)
   }
 
   process.on("SIGINT", () => disconnect())
   process.on("SIGTERM", () => disconnect())
   process.on("uncaughtException", (err) => {
     console.error(err)
-    disconnect()
+    disconnect(1)
   })
 }
