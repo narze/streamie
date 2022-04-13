c5e8b32d0ce17219930d0ea03c2665fe256e7f96
Manassarn Manoonchai <manassarn@gmail.com>
2022-04-13 16:16:09 +0700

    feat: add place page

---
+++ web/src/routes/place.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  const width = 32
+  const height = 32
+
+  const board: string[][] = Array(height)
+    .fill(0)
+    .map(() => Array(width).fill(""))
+
+  function paint(x, y, colorHex) {
+    board[y][x] = colorHex
+  }
+</script>
+
+<main>
+  {#each board as row, rowIndex}
+    <div class="flex">
+      {#each row as cell, cellIndex}
+        <div
+          class="flex flex-row border h-12 w-12 border-gray-100"
+          style={`background-color: #${cell}`}
+          on:click={() =>
+            paint(cellIndex, rowIndex, Math.floor(Math.random() * 16777215).toString(16))}
+        >
+          {cell}
+        </div>
+      {/each}
+    </div>
+  {/each}
+</main>
+
+<style>
+  :global(html, body) {
+    background-color: rgba(0, 0, 0, 0) !important;
+  }
+</style>
