68468fe2d64ef48db9c86232d35d75636dc955dc
Manassarn Manoonchai <manassarn@gmail.com>
2022-04-23 01:24:48 +0700

    feat: /coin command

---
+++ bot/src/commands/coin.ts
@@ -0,0 +1,25 @@
+import { SlashCommandBuilder } from "@discordjs/builders"
+import prisma from "../prisma"
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("coin")
+    .setDescription("Query for your $OULONG"),
+  async execute(interaction) {
+    const discordId = interaction.user.id
+    const discordUsername = interaction.user.username
+
+    // Get user by discordId
+    const user = await prisma.user.findFirst({
+      where: { discordId },
+    })
+
+    if (user) {
+      await interaction.reply(`คุณ ${discordUsername} มี ${user.coin} $OULONG`)
+    } else {
+      await interaction.reply(
+        `คุณยังไม่ได้ผูกบัญชี Twitch กับ Discord [TODO: login url]`
+      )
+    }
+  },
+}
