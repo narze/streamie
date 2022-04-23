62003d83483c9b500533ffc79745d235b6517118
Manassarn Manoonchai <manassarn@gmail.com>
2022-04-23 16:07:56 +0700

    fix: use ephemeral message

--- bot/src/commands/coin.ts
+++ bot/src/commands/coin.ts
@@ -1,11 +1,12 @@
 import { SlashCommandBuilder } from "@discordjs/builders"
+import { CommandInteraction } from "discord.js"
 import prisma from "../prisma"
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("coin")
     .setDescription("Query for your $OULONG"),
-  async execute(interaction) {
+  async execute(interaction: CommandInteraction) {
     const discordId = interaction.user.id
     const discordUsername = interaction.user.username
 
@@ -17,9 +18,9 @@ module.exports = {
     if (user) {
       await interaction.reply(`คุณ ${discordUsername} มี ${user.coin} $OULONG`)
     } else {
-      await interaction.reply(
-        `คุณยังไม่ได้ผูกบัญชี Twitch กับ Discord -> https://streamie-public.narze.live/pub`
-      )
+      const content = `คุณยังไม่ได้ผูกบัญชี Twitch กับ Discord -> https://streamie-public.narze.live/pub`
+
+      await interaction.reply({ content, ephemeral: true })
     }
   },
 }
