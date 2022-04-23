3b6f1c5e39a4588133aca49d264a8153ad5e8ece
Manassarn Manoonchai <manassarn@gmail.com>
2022-04-23 15:20:55 +0700

    fix: try emitting without connect

--- web/src/routes/pub/discord/connect_twitch.ts
+++ web/src/routes/pub/discord/connect_twitch.ts
@@ -2,22 +2,23 @@ import type { RequestHandler } from "@sveltejs/kit"
 import { io } from "socket.io-client"
 const VITE_STREAMIE_SOCKET_IO_SERVER_URL = import.meta.env.VITE_STREAMIE_SOCKET_IO_SERVER_URL
 
+const socket = io(VITE_STREAMIE_SOCKET_IO_SERVER_URL)
+
 export const post: RequestHandler = async (event) => {
   const payload = await event.request.json()
 
   console.log({ payload })
 
   // TODO: Extract to env
-  const socket = io(VITE_STREAMIE_SOCKET_IO_SERVER_URL)
 
-  const promise = new Promise((resolve, _reject) => {
-    socket.on("connect", async () => {
-      await socket.emit("discord_connect_twitch", payload)
-      resolve("sent")
-    })
-  })
+  // const promise = new Promise((resolve, _reject) => {
+  // socket.on("connect", async () => {
+  await socket.emit("discord_connect_twitch", payload)
+  // resolve("sent")
+  // })
+  // })
 
-  await promise
+  // await promise
 
   return { body: "TODO" }
 }
